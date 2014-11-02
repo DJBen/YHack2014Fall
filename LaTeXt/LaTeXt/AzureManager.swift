2eff4e9ca7ecd008504c8897f05d06878fd57588
DJBen <lsh32768@gmail.com>
2014-11-02 01:31:18 -0500

    Median Stage of updating UI

---
+++ LaTeXt/LaTeXt/AzureManager.swift
@@ -0,0 +1,13 @@
+//
+//  AzureManager.swift
+//  LaTeXt
+//
+//  Created by Sihao Lu on 11/1/14.
+//  Copyright (c) 2014 DJ.Ben. All rights reserved.
+//
+
+import UIKit
+
+class AzureManager: NSObject {
+   
+}
---
+++ LaTeXt/LaTeXt/YHMessage.swift
@@ -0,0 +1,13 @@
+//
+//  YHMessage.swift
+//  LaTeXt
+//
+//  Created by Sihao Lu on 11/2/14.
+//  Copyright (c) 2014 DJ.Ben. All rights reserved.
+//
+
+import UIKit
+
+class YHMessage: NSObject {
+   
+}
